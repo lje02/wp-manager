50523c3a9ecbd0cc38322bc69cf9066f939046a2
lje02 <154250786+lje02@users.noreply.github.com>
2025-12-26 13:06:45 -0500

    Update wp-manager.sh

--- wp-manager.sh
+++ wp-manager.sh
@@ -1942,7 +1942,7 @@ function db_admin_tool() {
 function show_menu() {
     clear
     echo -e "${GREEN}=== Docker 智能部署系统 ($VERSION) ===${NC}"
-    echo -e "${GREEN}=== 仅供个人使用，请勿用于生产 ===${NC}" 
+    echo -e "${RED}=== 仅供个人使用，请勿用于生产 ===${NC}" 
 	echo "----------------------------------------------------------------"
     
     # --- 1. 部署中心 ---
